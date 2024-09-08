65ecf2e0efacf67bc3e3b69a1f315b232ef0af0b
Muhibi-Uthman <156141961+Muhibi-Uthman@users.noreply.github.com>
2024-09-08 21:09:30 +0100

    adding first project

---
+++ variables.js
@@ -0,0 +1,15 @@
+// let + const
+
+const obj = {
+	player: "bobby"
+	experience: 100
+	wizardLevel: false
+}
+
+const player = player.obj
+const experience = experience.obj
+let wizardLevel = wizardLevel.obj
+
+const {player, experience} = obj;
+
+const {wizardLevel} = obj;
